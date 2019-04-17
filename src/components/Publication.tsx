b7861d3d400a9109d3de9c7caf7aa1d3f0510416
Pyry Pennanen <pyry.p1@gmail.com>
2019-04-17 11:20:54 +0300

    Change publication view to statefull

--- src/components/Publication.tsx
+++ src/components/Publication.tsx
@@ -1,120 +1,118 @@
 import * as React from 'react';
-import * as PropTypes from 'prop-types';
-//import Select from 'react-select';
 
-const Publication = (
-    {title, code, doi, id, crossref = {}, tags = [""], notes = [], labels, quotes, done = false, edit = false, handleUpdatePublicationTitle, handleUpdatePublicationEdit, handleUpdatePublicationTags, handleUpdatePublicationNotes, handleTogglePublication, handleDeletePublication}
-  ) => {
-    function handleToggleNameEdit(toggle) {
-      handleUpdatePublicationEdit(id, toggle)
+class Publication extends React.Component
+  <any, any>
+  {
+    public static defaultProps = {
+        tags: [],
+        notes: []
     };
 
-    function _handleUpdatePublicationNotes(id, nth, newValue){
+    constructor(props) {
+      super(props);
+    }
+
+    state = {
+      showquotes:  false,
+    }
+
+    handleToggleNameEdit(toggle) {
+      this.props.handleUpdatePublicationEdit(this.props.id, toggle)
+    }
+
+    _handleUpdatePublicationNotes(id, nth, newValue){
       let newNotes : any[] = []
-      notes.forEach(function(val,idx) {
+      this.props.notes.forEach(function(val,idx) {
          nth==idx ? newNotes.push(newValue) : newNotes.push(val)
       })
-      handleUpdatePublicationNotes(id, newNotes)
+      this.props.handleUpdatePublicationNotes(id, newNotes)
     }
 
-    function _handleDeletePublicationNote(id, nth){
+    _handleDeletePublicationNote(id, nth){
       let newNotes : any[] = []
-      notes.forEach(function(val,idx) {
+      this.props.notes.forEach(function(val,idx) {
          if(nth!=idx){newNotes.push(val)}
       })
-      handleUpdatePublicationNotes(id, newNotes)
+      this.props.handleUpdatePublicationNotes(id, newNotes)
     }
 
-    function _handleAddPublicationNote(id){
-      handleUpdatePublicationNotes(id, [...notes, [""]])
+    _handleAddPublicationNote(id){
+      this.props.handleUpdatePublicationNotes(id, [...this.props.notes, [""]])
     }
 
-    function _handleCrossRef(){
-      fetch('https://api.crossref.org/works/'+doi)
+    _handleCrossRef(){
+      fetch('https://api.crossref.org/works/'+this.props.doi)
         .then(result=>result.json())
         .then(items=>{console.log(items);alert(items.message['is-referenced-by-count']);})
     }
 
-    return (<li>
-  <h3 onClick={(e) => handleToggleNameEdit(!edit)}>{title}</h3>
-  <p style={{marginTop : '-1em'}}>
-    {"id:" + id + ", "}
-    {
-    "notes:" + notes.length + ", " + labels.filter((i) => (tags.includes(i.id.toString()))).map((i) => (i.title+" ")) +
-    crossref['author'][0]['family'] + " - " + crossref['container-title'][0]
-  }</p>
-  <div className="quotes__item--publication">{(
-    quotes.filter((i) => (i.publicationId == id)).map((i) => (
-      <div key={'quotewrap-'+i.id} className="quotes__item-wrap">
-        {(i.labels ? (<span>{(i.labels || "")}</span>):null)}
-        <div key={'quote-'+i.id} style={{border: '1px solid darkgrey', backgroundColor: 'lightgrey', padding: '.5em'}}
-          title={i.labels + "\n" + i.desc} className="quotes__item">
-        {i.text}
+    onToggle = (e, name) => {
+      this.setState(prevState => ({
+        [name]: !prevState[name],
+      }));
+    }
+
+    render = () => {return (<li>
+    <h3 onClick={(e) => this.handleToggleNameEdit(!this.props.edit)}>{this.props.title}</h3>
+    <p style={{marginTop : '-1em'}}>
+      <button name={'showquotes'}
+        onClick={(e) => this.onToggle(e, 'showquotes')}
+        style={{
+          color : this.state.showquotes ? 'green':'black',
+          display : this.props.quotes.filter((i) => (i.publicationId == this.props.id)).length ? 'inline-block': 'none'
+        }}>Show quotes {this.props.quotes.filter((i) => (i.publicationId == this.props.id)).length}</button>&nbsp;
+      {"id:" + this.props.id + ", "}
+      {
+      "notes:" + (this.props.notes ? this.props.notes.length : '0') + ", " + this.props.labels.filter((i) => (this.props.tags.includes(i.id.toString()))).map((i) => (i.title+" ")) +
+      this.props.crossref['author'][0]['family'] + " - " + this.props.crossref['container-title'][0]
+    }</p>
+    {(this.state.showquotes ? (
+    <div className="quotes__item--publication">{(
+      this.props.quotes.filter((i) => (i.publicationId == this.props.id)).map((i) => (
+        <div key={'quotewrap-'+i.id} className="quotes__item-wrap">
+          {(i.labels ? (<span>{(i.labels || "")}</span>):null)}
+          <div key={'quote-'+i.id} style={{border: '1px solid darkgrey', backgroundColor: 'lightgrey', padding: '.5em'}}
+            title={i.labels + "\n" + i.desc} className="quotes__item">
+          {i.text}
+          </div>
         </div>
-      </div>
-    ))
-  )}
-  </div>
-  {(edit ? (<div><input
-    name="title"
-    type="text"
-    value={title}
-    onChange={(e) => handleUpdatePublicationTitle(id, e.target.value)}
-  />
-  <input
-    name="done"
-    type="checkbox"
-    checked={done}
-    onChange={(e) => handleTogglePublication(id, !done)}
-  />
-  <div>
-    <button type="button" onClick={() =>  _handleCrossRef()}>Check Citation(1_sec_lim)</button>
-  </div>
-  <div><h4>Notes:</h4>
-  {notes.map((i, idx) => (
-    <span key={'span' + idx}>
-    <textarea key={idx} value={i} onChange={(e) => _handleUpdatePublicationNotes(id, idx, e.target.value)} />
-    <input type="checkbox" onChange={(e) => _handleDeletePublicationNote(id, idx)} />
-    </span>
-  ))}
-  <button type="button" onClick={() =>  _handleAddPublicationNote(id)}>Add note</button></div>
-  <select
-    name="tags"
-    defaultValue={tags}
-    multiple
-    onChange={(e) =>  handleUpdatePublicationTags(id, [].filter.call(e.target.options, o => o.selected).map(o => o.value))}>
-    {labels.map((i) => (<option key={i.id} value={i.id}>{i.title}</option>))}
-  </select>
-  <button type="button" onClick={() => handleDeletePublication(id)}>Delete</button>
-</div>):null)}</li>)
-}
-
-/*
-{labels.map((i) => (<option key={i.id} value={i.id} selected={tags && tags.includes(i.id)}>{i.title}</option>))}
-onChange={(e) => handleSelectPublicationLabel(id, e.target.value)}
-<Select
-      value={tags}
-      onChange={(e) => handleSelectPublicationLabel(id, e.target.value)}
-      options={labels}
+      ))
+    )}
+    </div>
+    ):null)}
+    {(this.props.edit ? (<div><input
+      name="title"
+      type="text"
+      value={this.props.title}
+      onChange={(e) => this.props.handleUpdatePublicationTitle(this.props.id, e.target.value)}
     />
-*/
-
-Publication.propTypes = {
-  title: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
-  code: PropTypes.string,
-  doi: PropTypes.string,
-  tags: PropTypes.array,
-  notes: PropTypes.array,
-  labels: PropTypes.array,
-  quotes: PropTypes.array,
-  done: PropTypes.bool,
-  toggleEdit: PropTypes.bool,
-  handleUpdatePublicationTitle: PropTypes.func.isRequired,
-  handleUpdatePublicationTags: PropTypes.func.isRequired,
-  handleUpdatePublicationNotes: PropTypes.func.isRequired,
-  handleTogglePublication: PropTypes.func.isRequired,
-  handleDeletePublication: PropTypes.func.isRequired
-};
+    <input
+      name="done"
+      type="checkbox"
+      checked={this.props.done}
+      onChange={(e) => this.props.handleTogglePublication(this.props.id, !this.props.done)}
+    />
+    <div>
+      <button type="button" onClick={() =>  this._handleCrossRef()}>Check Citation(1_sec_lim)</button>
+    </div>
+    <div><h4>Notes:</h4>
+    {this.props.notes.map((i, idx) => (
+      <span key={'span' + idx}>
+      <textarea key={idx} value={i} onChange={(e) => this._handleUpdatePublicationNotes(this.props.id, idx, e.target.value)} />
+      <input type="checkbox" onChange={(e) => this._handleDeletePublicationNote(this.props.id, idx)} />
+      </span>
+    ))}
+    <button type="button" onClick={() =>  this._handleAddPublicationNote(this.props.id)}>Add note</button></div>
+    <select
+      name="tags"
+      defaultValue={this.props.tags}
+      multiple
+      onChange={(e) =>  this.props.handleUpdatePublicationTags(this.props.id, [].filter.call(e.target.options, o => o.selected).map(o => o.value))}>
+      {this.props.labels.map((i) => (<option key={i.id} value={i.id}>{i.title}</option>))}
+    </select>
+    <button type="button" onClick={() => this.props.handleDeletePublication(this.props.id)}>Delete</button>
+  </div>):null)}</li>);
+  }
+}
 
 export default Publication;
