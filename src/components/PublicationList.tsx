552536b7669696f0f0e9a09677be5b2ad41e9d59
Pyry Pennanen <pyry.p1@gmail.com>
2019-05-15 20:09:15 +0300

    Add select to filter publications by labels in references quotes

--- src/components/PublicationList.tsx
+++ src/components/PublicationList.tsx
@@ -1,4 +1,5 @@
 import * as React from 'react';
+import Select from 'react-select';
 
 import Publication from './Publication';
 
@@ -14,15 +15,51 @@ class PublicationList extends React.Component
 
   constructor(props) {
     super(props);
+    this.handleMultiChange = this.handleMultiChange.bind(this);
   }
 
   state = {
     filter:  [],
   }
+  handleMultiChange(option) {
+    this.setState(state => {
+      return {
+        filter: option
+      };
+    });
+  }
+  statefiltered = () => {
+    // At the moment we are only looking at match to labels in attached quotes
+    function common(a, b) {
+      return b.filter(Set.prototype.has.bind(new Set(a)));
+    }
+    let matchingLabels = this.props.labels.filter((l)=>{
+      let isSelected = this.state.filter.map((f)=>Number(f['value'])).includes(Number(l.id));
+      let childFilters = this.state.filter.filter((f)=>(String(l.title).toLowerCase().indexOf(String(f['label']).toLowerCase())>-1));
+      return (isSelected || childFilters.length > 0);
+    });
+    let matchingLabelTexts = matchingLabels.map((ml)=>ml.title);
+    let matchingQuotes = this.props.quotes.filter((q)=>{
+      return [q.labels, matchingLabelTexts].reduce(common).length > 0;
+    })
+
+    return this.props.publications.filter((p)=>{
+      if(this.state.filter.length === 0) {return true};
+      if(matchingQuotes.filter((q)=>q.publicationId === p.id).length > 0) {return true};
+      return false;
+    });
+  }
 
   render = () => { return (
+    <div>
+    <Select
+        value={this.state.filter}
+        onChange={this.handleMultiChange}
+        isMulti
+        options={this.props.labels.map((l)=>{return {value:l.id,label:l.title}})}
+      />
     <ul style={{paddingLeft : '0'}}>
-      {this.props.publications
+      {this.statefiltered()
         .filter((i) => (!this.props.filters.includes(i.state)))
         .filter((i)=>(
           (!this.props.filters.includes('forums') && i.forum) || this.props.filters.includes('forums')
@@ -42,7 +79,7 @@ class PublicationList extends React.Component
         handleUpdatePublicationNotes={this.props.handleUpdatePublicationNotes}
         handleDeletePublication={this.props.handleDeletePublication}
       />)}
-    </ul>
+    </ul></div>
   )}
 }
 
